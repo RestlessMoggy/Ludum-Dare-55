97820867af90cb708806ee1819e6e15a3a779cbe
Andreas Aditya <adit@pop-os.localdomain>
2024-04-16 04:21:32 +0700

    cleanup

--- scripts/game_over.gd
+++ scripts/game_over.gd
@@ -1,12 +1,6 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$TextureRect.position -= Vector2(4, 4)
 	if $TextureRect.position.x <= -256:
@@ -19,4 +13,4 @@ func _on_quit_button_pressed():
 
 
 func _on_restart_button_pressed():
-	get_tree()
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
